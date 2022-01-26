24975370643de8eda9f6ea575e282aa0178f3b2d
Nicholas Barros <81240462+NickRyanDev@users.noreply.github.com>
2022-01-26 14:19:04 -0300

    Correct path of url

--- books/books/urls.py
+++ books/books/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from DisplayBooks import GetBooks
+from DispayBooks.view import GetBooks
 
 urlpatterns = [
     path('admin/', admin.site.urls),
